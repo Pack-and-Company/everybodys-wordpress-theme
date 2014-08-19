9cc5eebbfd5120888d4943157050fef7b9ca4779
Tom Henderson <tomhenderson@mac.com>
2014-08-20 10:09:43 +1200

    Add nav menu generator.

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
 <?php get_header(); ?>
 
+<?php
+
+$args = array(
+    'post_type'   => 'post',
+    'post_status' => 'publish',
+    'orderby'     => 'menu_order',
+    'order'       => 'ASC',
+);
+$menu_data = get_posts( $args );
+
+function eb_get_nav_menu($menu_data) {
+	echo '<ul class="nav">';
+	$item_format = '    <li><a class="icon-%s" href="#%s">%s</a></li>';
+	foreach ( $menu_data as $menu_item ) {
+		sprintf($item_format, $menu_item->post_name, $menu_item->post_name, $menu_item->the_title)
+	}
+	echo '</ul>';
+}
+
+?>
+
 <?php if ( have_posts() ): ?>
   <?php while ( have_posts() ) : the_post(); ?>
 
@@ -7,12 +28,13 @@
 	<div class="wrapper">
 		<div class="container menu">
 			<a href="#home"><img src="<?=get_template_directory_uri();?>/images/logo.png"></a>
-			<ul class="nav">
+			<?php echo eb_get_nav_menu(); ?>
+			<!-- <ul class="nav">
 				<li><a class="icon-menu" href="#menus">Menus</a></li>
 				<li><a class="icon-phone" href="#contact">Contact</a></li>
 				<li><a class="icon-pencil" href="#bookings">Bookings</a></li>
 				<li><a class="icon-camera" href="#gallery">Gallery</a></li>
-			</ul>
+			</ul> -->
 		</div>
 		<div class="container main">
 			<div class="content">
