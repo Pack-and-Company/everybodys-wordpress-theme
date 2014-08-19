dbae6e51adb260fe557545ef85927d3117562085
Tom Henderson <tomhenderson@mac.com>
2014-08-19 16:49:41 +1200

    Comments in script.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
 jQuery(document).ready(function($){
+	// Add selected class if nav menu target matches id of containing page element
 	$('.page').each(function(){
 		$(this).find("a[href='#" + $(this).attr('id') + "']").addClass('selected');
 	});
 
+	// Add pdf class to links to pdf files
 	$('a[href$=pdf').addClass('pdf');
 
+	// Override anchor links to animate scrolling
 	$(".menu a").click(function(e) {
 		e.preventDefault();
 		$('html, body').animate({
