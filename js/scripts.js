2c36ee81ccdae2ee25e0063d09325bf5530befc2
Tom Henderson <tomhenderson@mac.com>
2014-08-19 16:22:54 +1200

    Select current menu item.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
 jQuery(document).ready(function($){
+	$('.page').each(function(){
+		$(this).find("a[href='#" + $(this).attr('id') + "']").addClass('selected');
+	});
+
 	$(".menu a").click(function(e) {
 		e.preventDefault();
 		$('html, body').animate({
 	        scrollTop: $( $(this).attr('href') ).offset().top
 	    }, 1000, 'easeInOutQuart');
-	});	
+	});
 });
