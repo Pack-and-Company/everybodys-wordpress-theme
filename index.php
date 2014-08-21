8b8f98c117595fa31f72019cb46a3244bd520eb3
Tom Henderson <tomhenderson@mac.com>
2014-08-21 16:08:15 +1200

    Check if there are any tags before looping through them.

--- index.php
+++ index.php
@@ -39,45 +39,48 @@ function eb_get_nav_menu($menu_data) {
 				<h2><?php the_title(); ?></h2>
 				<?php the_content(); ?>
 				<?php
-					foreach ( get_the_tags() as $tag ) {
-						if ( $tag->name == 'events' ) {
-							?>
-							<div class="events-list">
+					$posttags = get_the_tags();
+					if ($posttags) {
+						foreach ( $posttags as $tag ) {
+							if ( $tag->name == 'events' ) {
+								?>
+								<div class="events-list">
+									<?php
+
+					                $args = array(
+					                    'post_type'   => 'events',
+					                    'post_status' => 'publish',
+					                    'orderby'     => 'menu_order',
+					                    'order'       => 'ASC',
+					                );
+					                $events = get_posts( $args );
+
+					                foreach ( $events as $event ) {
+
+					                    setup_postdata($event);
+				                        printf('<dl class="event-item">');
+										printf(
+											'<dt class="event-image"><a href="%s" title="%s"><img src="%s" width="190" height="269" alt="%s"></a></dt>',
+											wp_get_attachment_url(get_post_thumbnail_id($event->ID)),
+											$event->post_content,
+											wp_get_attachment_url(get_post_thumbnail_id($event->ID)),
+											$event->post_title
+										);
+										printf(
+											'<dd class="event-info"><strong>%s</strong><br>%s, %s, %s<br><p>%s</p></dd>',
+											$event->post_title,
+											get_post_meta($event->ID, '_event_date', true),
+											get_post_meta($event->ID, '_event_time', true),
+											get_post_meta($event->ID, '_event_price', true),
+											$event->post_content
+										);
+				                        printf('</dl>');
+					                }
+					                #wp_reset_postdata();
+					                ?>
+								</div>
 								<?php
-
-				                $args = array(
-				                    'post_type'   => 'events',
-				                    'post_status' => 'publish',
-				                    'orderby'     => 'menu_order',
-				                    'order'       => 'ASC',
-				                );
-				                $events = get_posts( $args );
-
-				                foreach ( $events as $event ) {
-
-				                    setup_postdata($event);
-			                        printf('<dl class="event-item">');
-									printf(
-										'<dt class="event-image"><a href="%s" title="%s"><img src="%s" width="190" height="269" alt="%s"></a></dt>',
-										wp_get_attachment_url(get_post_thumbnail_id($event->ID)),
-										$event->post_content,
-										wp_get_attachment_url(get_post_thumbnail_id($event->ID)),
-										$event->post_title
-									);
-									printf(
-										'<dd class="event-info"><strong>%s</strong><br>%s, %s, %s<br><p>%s</p></dd>',
-										$event->post_title,
-										get_post_meta($event->ID, '_event_date', true),
-										get_post_meta($event->ID, '_event_time', true),
-										get_post_meta($event->ID, '_event_price', true),
-										$event->post_content
-									);
-			                        printf('</dl>');
-				                }
-				                #wp_reset_postdata();
-				                ?>
-							</div>
-							<?php
+							}
 						}
 					}
 				?>
